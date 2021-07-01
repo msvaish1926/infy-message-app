350a719126228f82fc3ffbc64da91d0a6466fda2
msvaish1926 <73881530+msvaish1926@users.noreply.github.com>
2021-07-01 21:07:24 +0530

    added queue env variable to the publisher app

--- infy-publisher-app/app.js
+++ infy-publisher-app/app.js
@@ -7,6 +7,7 @@ var amqp = require("amqplib/callback_api");
 var uuid = require("uuid");
 var randomColor = require("randomcolor");
 var rabbitMQURL = process.env.RABBITMQURL ? "amqp://"+process.env.RABBITMQURL : "amqp://localhost";
+var queueName = process.env.RABBITMQ_QUEUE || "infyMessageQueue";
 app.use(bodyParser.json());
 app.use(fileupload());
 
@@ -24,7 +25,6 @@ app.post("/publisher/api/send-message", function (req, res) {
 			if(err){
 				throw err;
 			}
-			let queueName = "publisherQueue";
 			let messagJson = JSON.parse(req.files.publish.data.toString()).map(e=>{
 				e.sendTime = Date.now();
 				e.initials = e.firstname.substring(0,1)+e.lastname.substring(0,1);
